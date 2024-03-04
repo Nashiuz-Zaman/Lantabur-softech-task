955ca5d61aa8f80ef5ec2320cf36cb90ee2dcaa7
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-03-04 23:57:07 +0600

    Add reusable login and registration forms

---
+++ src/components/forms/LoginModal/LoginModal.jsx
@@ -0,0 +1,55 @@
+'use client';
+
+// react
+import { useEffect, useCallback } from 'react';
+
+// components
+import LoginFormWithImage from '../LoginFormWithImage/LoginFormWithImage';
+
+// hooks
+import useClickOutside from '@/hooks/useClickOutside';
+import useFormVisiblity from '@/hooks/useFormVisiblity';
+import useStopScrolling from '@/hooks/useStopScrolling';
+
+// redux
+import { useSelector } from 'react-redux';
+
+// data
+import loginImg from './../../../assets/forms/login.webp';
+
+const LoginModal = () => {
+   const { loginFormOpen } = useSelector(store => store.form);
+   const { closeLoginFormWithBackdrop } = useFormVisiblity();
+   const { stopYAxisScrolling } = useStopScrolling();
+
+   useEffect(() => {
+      stopYAxisScrolling(loginFormOpen);
+   }, [loginFormOpen, stopYAxisScrolling]);
+
+   const handleClickOutside = useCallback(
+      e => {
+         if (
+            e.target.closest('.login-custom-focus') ||
+            e.target.closest('.password-custom-focus')
+         ) {
+            return;
+         }
+
+         closeLoginFormWithBackdrop();
+      },
+      [closeLoginFormWithBackdrop]
+   );
+   useClickOutside(loginFormOpen, handleClickOutside);
+
+   return (
+      <div
+         className={`fixed w-full z-40 transition-all duration-default opacity-0 collapse top-1/2 left-1/2 -translate-y-full -translate-x-1/2 ${
+            loginFormOpen ? '!opacity-100 !visible !-translate-y-1/2' : ''
+         }`}
+      >
+         <LoginFormWithImage imageSource={loginImg} />
+      </div>
+   );
+};
+
+export default LoginModal;
