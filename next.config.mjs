86394c6be8c05e26e537639bdf698edba6218ecf
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-03-04 23:41:28 +0600

    Add website logos to project and env variables to next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+   env: {
+      DB_USER: process.env.DB_USER,
+      DB_PASS: process.env.DB_PASS,
+      DB_NAME: process.env.DB_NAME,
+      NEXT_PUBLIC_PROD_SERVER_URL: process.env.NEXT_PUBLIC_PROD_SERVER_URL,
+   },
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'https',
+            hostname: 'i.ibb.co',
+            port: '',
+            pathname: '/**',
+         },
+      ],
+   },
+};
 
 export default nextConfig;
