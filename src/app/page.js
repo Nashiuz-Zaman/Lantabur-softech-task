1f1b473e2574e9ea2c5de23f37ae0ed4f762f8d6
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-03-04 19:41:58 +0600

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
+
+    fetch('http://localhost:3000/api/').then(res => res.json()).then(data => data)
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,98 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: [
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+   theme: {
+      extend: {
+         screens: {
+            '2xs': '360px',
+            xs: '480px',
+            '2md': '850px',
+            '3xl': '1700px',
+         },
+         keyframes: {
+            'fade-in': {
+               '0%': { opacity: '0' },
+               '100%': { opacity: '1' },
+            },
+            'fade-in-from-right': {
+               '0%': {
+                  opacity: '0',
+                  position: 'relative',
+                  transform: 'translateX(20%)',
+               },
+               '100%': {
+                  opacity: '1',
+                  position: 'relative',
+                  transform: 'translateX(0)',
+               },
+            },
+            'fade-in-from-left': {
+               '0%': {
+                  opacity: '0',
+                  position: 'relative',
+                  transform: 'translateX(-20%)',
+               },
+               '100%': {
+                  opacity: '1',
+                  position: 'relative',
+                  transform: 'translateX(0)',
+               },
+            },
+         },
+         animation: {
+            fadeIn: 'fade-in 0.3s ease-out forwards',
+            fadeInFromRight: 'fade-in-from-right 0.7s ease-out forwards',
+            fadeInFromLeft: 'fade-in-from-left 0.7s ease-out forwards',
+         },
+         spacing: {
+            custom4xl: '20rem',
+            custom3xl: '17rem',
+            custom2xl: '15rem',
+            customXl: '13rem',
+            customLg: '11rem',
+            custom3md: '9rem',
+            custom2md: '7.5rem',
+            customMd: '6rem',
+            customSm: '4.2rem',
+            customXs: '2.85rem',
+            custom2xs: '1.7rem',
+            custom3xs: '0.8rem',
+         },
+         boxShadow: {
+            large: '0 10px 60px -10px rgba(0,0,0,0.25)',
+            medium: '0 5px 40px -5px rgba(0,0,0,0.25)',
+            small: '0 3px 20px -3px rgba(0,0,0,0.2)',
+         },
+         transitionDuration: {
+            default: '150ms',
+         },
+         colors: {
+            primary: '#ff8631',
+            secondary: '#DB5937',
+            primaryDark: '#e6792c',
+            primaryLight: '#ff9246',
+            primaryLightest: '#ffdbc1',
+            textPrimary: '#1C1B1B',
+            textMediumLight: '#1c1b1bcc',
+            textLight: '#1c1b1b99',
+            lightGray: '#f5f5f5',
+            lightBorder: '#ddd',
+            blackLight: '#111',
+            darkThemeBg: '#202124',
+         },
+
+         borderRadius: {
+            default: '5px',
+            defaultLg: '10px',
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [
+      // ...
+      require('tailwind-scrollbar')({ nocompatible: true }),
+   ],
 };
